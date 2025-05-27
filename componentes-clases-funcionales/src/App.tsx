a07d1b45d83bf1fd948c67596002935e6280be6b
KevinJulianGR <kevin.julian.372@gmail.com>
2025-05-27 10:15:03 -0500

    creation class component

--- componentes-clases-funcionales/src/App.tsx
+++ componentes-clases-funcionales/src/App.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { ComponenteFuncional } from './Components/ComponenteFuncional';
+import { ComponenteClase } from './Components/ComponenteClase';
 
 function App() {
   return (
     <>
     <ComponenteFuncional name='Kevin Julian'/>
+    <br/>
+    <ComponenteClase name='Kevin Julian'/>
     </>
   );
 }
