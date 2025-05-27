ebb24309fb91b43b9a301f4d3d5bece053b196aa
KevinJulianGR <kevin.julian.372@gmail.com>
2025-05-27 09:41:49 -0500

    creacion primer componente

---
+++ creacion-componentes/src/components/HolaMundo.tsx
@@ -0,0 +1,12 @@
+interface HolaMundoProps{
+
+}
+
+export const HolaMundo : React.FC<HolaMundoProps> =({})=>{
+    
+    return(
+        <>
+        Hola Mundo desde un componente
+        </>
+    ) 
+}
