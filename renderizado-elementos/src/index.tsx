df18fc65b52149a502b786bbf8e621af2b0b1038
KevinJulianGR <kevin.julian.372@gmail.com>
2025-05-26 22:10:02 -0500

    DOM para el renderizado de elementos

--- renderizado-elementos/src/index.tsx
+++ renderizado-elementos/src/index.tsx
@@ -4,16 +4,31 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+// const root = ReactDOM.createRoot(
+//   document.getElementById('root') as HTMLElement
+// );
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+
+const root2 = ReactDOM.createRoot(
+  document.getElementById('root2') as HTMLElement
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+const tick = () => {
+  const element = <div>
+    <h1>Hello, World</h1>
+    <h2>It's {new Date().toLocaleTimeString()}</h2>
+  </div>
+
+
+  root2.render(element);
+}
+
+setInterval(tick, 1000);
+
+
 reportWebVitals();
