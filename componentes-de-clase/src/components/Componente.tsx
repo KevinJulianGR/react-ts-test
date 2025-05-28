85aa55ed1725ff421c14e5e529439043a92a87a9
KevinJulianGR <kevin.julian.372@gmail.com>
2025-05-28 10:06:43 -0500

    componentes props y state

--- componentes-de-clase/src/components/Componente.tsx
+++ componentes-de-clase/src/components/Componente.tsx
@@ -17,8 +17,30 @@ export class Componente extends React.Component<ComponenteProps, ComponentState>
     render(){
         console.log("render")
         return <>
-            Hola {this.state.name}
+            Hola, {this.state.name}
+            <br />
+            <button onClick={()=>{this.setState({name: "Julian"})}}>
+                Cambiar Nombre 
+            </button>
         </>
+    }
+    componentDidMount() {
+        console.log("ComponentDidMount");
+    
+    }
+    componentDidUpdate(prevProps: Readonly<ComponenteProps>, prevState: Readonly<ComponentState>, snapshot?: any){
+        console.log("ComponentDidUpdate")
+        console.log("prevProps");
+        console.log(JSON.stringify(prevProps));
+        console.log("Current props");
+        console.log(JSON.stringify(this.props));
+        console.log("prevStae");
+        console.log(JSON.stringify(prevState));
+        console.log("Current state");
+        console.log(JSON.stringify(this.state));
+    }
+    componentWillUnmount(){
+        console.log("ComponentWillMount");
     }
 }
 
