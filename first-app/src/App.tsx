f81117d3f8f722434141bc006c87c98f9bbd7800
KevinJulianGR <kevin.julian.372@gmail.com>
2025-05-29 15:25:27 -0500

    Estado

--- first-app/src/App.tsx
+++ first-app/src/App.tsx
@@ -4,10 +4,14 @@ import List from "./components/List";
 function App() {
   const list =["Kevin", "Julian", "Andres"]
 
+  const handleSelect =(elemento:String)=>{
+    console.log("imprimiendo "+elemento )
+  }
+
   return (
     <Card>
       <CardBody title="Titulo" text="Texto" />
-      <List data={list}/>
+      <List data={list} onSelect={handleSelect}/>
     </Card>
   );
 }
